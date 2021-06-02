3e7c564323e916dea496702eed79bab70e98160d
MelbouVP <saurfanng@gmail.com>
2021-06-02 22:14:21 +0300

    Implemented: 1. Error boundary component to handle 404 pages; 2. Lazy loading for Cart page and PDP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component, Suspense, lazy } from 'react'
 import { Route, Switch } from "react-router-dom";
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
+
 import Navbar from './Components/Navbar/navbar.component'
 import ProductListingPage from './Pages/Product-listing-page/product-listing-page.component'
-import ProductPage from './Pages/Product-description-page/product-page.component'
-import CartPage from './Pages/Cart-page/cart-page.component'
+import ErrorBoundary from './Components/Error-boundary/error-boundary.component'
 import Spinner from './Components/Spinners/spinner.component'
+
 
 import { selectCurrencyHasLoaded } from './Redux/currency/currency.selectors'
 import { fetchCurrenciesStart } from './Redux/currency/currency.actions'
 import { fetchProductDataStart } from './Redux/shop/shop.actions'
 
 import './App.css';
+
+// load ProductPage and CartPage only when user proceeds to them
+const ProductPage = lazy(() => import('./Pages/Product-description-page/product-page.component'))
+const CartPage = lazy(() => import('./Pages/Cart-page/cart-page.component'))
+
 export class App extends Component {
 
   componentDidMount() {
@@ -26,21 +32,25 @@ export class App extends Component {
     const { currencyHasLoaded } = this.props 
 
     return (
-      <>
-        {
-          currencyHasLoaded ?
-            <div className="App">
-              <Navbar />
-              <Switch >
-                <Route exact path='/' component={ProductListingPage} />
-                <Route exact path='/cart' component={CartPage} />
-                <Route exact path='/product/:name' component={ProductPage} />
-              </Switch>
+          <div className="App">
+            <ErrorBoundary>
+              <Suspense fallback={<Spinner />}>
+              { 
+                currencyHasLoaded ? 
+                  <>
+                    <Navbar />
+                    <Switch >
+                      <Route exact path='/' component={ProductListingPage} />
+                      <Route exact path='/cart' component={CartPage} />
+                      <Route exact path='/product/:name' component={ProductPage} />
+                    </Switch>
+                  </>
+                :
+                  <Spinner />
+              }
+              </Suspense>
+            </ErrorBoundary>
             </div>
-          :
-            <Spinner />
-        }
-      </>
     )
   }
 }
